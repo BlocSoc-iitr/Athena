9000ffbc1ac468423124166ae252fd959005c271
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-09-16 00:47:33 +0530

    Added testing for encoding

--- athena_abi/cairo_type_decoders_test.go
+++ athena_abi/cairo_type_decoders_test.go
@@ -25,9 +25,14 @@ func TestArrayDecoding(t *testing.T) {
 		for i := 0; i < len(test.decoded); i++ {
 			assert.Equal(t, test.decoded[i], decodedValues[i])
 		}
-		// encodedCalldata, err := EncodeFromTypes([]StarknetType{test.starknetType}, []interface{}{test.decoded})
-		// assert.Equal(t, err, nil)
-		// assert.Equal(t, test.calldata, encodedCalldata)
-		// assert.Equal(t, 0, len(_calldata))
+		// needed to convert []*big.Int into []interface{}
+		interfaceSlice := make([]interface{}, len(test.decoded))
+		for i, v := range test.decoded {
+			interfaceSlice[i] = v
+		}
+		encodedCalldata, err := EncodeFromTypes([]StarknetType{test.starknetType}, []interface{}{interfaceSlice})
+		assert.Equal(t, nil, err)
+		assert.Equal(t, test.calldata, encodedCalldata)
+		assert.Equal(t, 0, len(_calldata))
 	}
 }
